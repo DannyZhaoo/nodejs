dfbd559052f5bee8c934198bfca220f2b18b073e
DannyZhaoo <xiudanzhao@outlook.com>
2017-06-05 17:49:03 +0800

    完成了对node搭建web的尝试，这个是简单的express，有助于理解部分原理。

--- node-first-web/requestHandlers.js
+++ node-first-web/requestHandlers.js
@@ -18,7 +18,6 @@ function start(response) {
     '</form>'+
     '</body>'+
     '</html>';
-
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
